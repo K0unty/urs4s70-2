3e04cad73919ae695d1ed206f835d1f2cfe8e467
K0unty <kountiapi@pm.me>
2024-07-10 06:49:27 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 function App() {
-  return <p>Coming Soon..</p>;
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">PLayer 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>EDIT</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">PLayer 1</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>EDIT</button>
+          </li>
+        </ol>
+        PLAYERS GAME BOARD
+      </div>
+    </main>
+  );
 }
 
 export default App;
